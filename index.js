1a5e212c3288b55df878f4fc82154ad7a7c814b3
Prachi Thakkar <prachi281194@gmail.com>
2019-05-07 10:33:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+  console.log('Server Port: 3000');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Session #2 - Deep Dive into AWS EC2');
 }).listen(3000);
